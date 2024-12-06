6630170f650dd834b507e7603704716fa804eb6a
jamisolarad <roseanndjamisola@gmail.com>
2024-12-06 12:29:56 +0000

    Tasksheet112

---
+++ Tasksheet112/PrimitivesAct.java
@@ -0,0 +1,20 @@
+package Tasksheet112;
+
+public class PrimitivesAct {
+    public static void main(String[] args) {
+        boolean bo = true;
+        char h = 'H';
+        char w = 'w';
+        char d = 'd';
+        char r = 'r';
+        byte by = 0;
+        int ello = 3110;
+        short s = 1;
+        float fl = 2.0f;
+
+        String result = h + "" + ello + " " + w + "" + by + "" + r + "" + s + "" + d + " " + fl + " " + bo;
+        System.out.println(result);
+
+    }
+    
+}

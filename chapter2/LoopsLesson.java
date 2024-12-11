bea05d2b0f09d058dea8cc7b3b38c9a174c15b4e
jamisolarad <roseanndjamisola@gmail.com>
2024-12-11 12:34:14 +0000

    Task114

--- chapter2/LoopsLesson.java
+++ chapter2/LoopsLesson.java
@@ -6,6 +6,15 @@ public class LoopsLesson {
         while(a<=10)
         System.out.println("a = " +a--);
         System.out.println("End of Program");
+
+        ROW_TABLE: for(int p = 1; p<=10; p++)
+        COL_TABLE:    for(int l = 1;l<=10; l++){
+                            if(l==5)
+                            break COL_TABLE;
+                            System.out.println(1*p+"\t");
+        }
+                         System.out.println();     
+      
     
     }
     

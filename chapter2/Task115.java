067bd598e9a1b95c42957673744bb7d5e31657ae
jamisolarad <roseanndjamisola@gmail.com>
2024-12-13 07:45:16 +0000

    Task115

---
+++ chapter2/Task115.java
@@ -0,0 +1,20 @@
+package chapter2;
+
+public class Task115 {
+    public static void main(String[] args) {
+        String a = "Wow";
+        String b = "Wow";
+        String c = new String("Wow!");
+        String d = "Wow!";
+
+        boolean b1 = a == b;
+        boolean b2 = d.equals(b + "!");
+        boolean b3 = !c.equals(a);
+
+        if (b1 && b2 && b3) {
+            System.out.println("Success!");
+        }
+
+    }
+    
+}
